dc0ff93a00f449efa024d2be948117d7f1217e05
RenanSczcepanik <renan.scz@hotmail.com>
2017-11-04 10:37:24 -0200

    Primeiro Commit

---
+++ database/migrations/2017_10_07_133036_create_imagens_pessoas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagensPessoasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imagens_pessoas', function (Blueprint $table) {
+            $table->integer('id_formulario_imagens')->unsigned();
+            $table->foreign('id_formulario_imagens')->references('id')->on('formulario_imagens');
+            $table->integer('id_pessoas')->unsigned();
+            $table->foreign('id_pessoas')->references('id')->on('pessoas');
+            $table->primary(['id_formulario_imagens', 'id_pessoas']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imagens_pessoas');
+    }
+}
